3b1e5b163144dd190840a6856e6ebee92a5e9c05
Jeff <jm@jeffz.com>
2021-09-07 02:45:48 -0400

    Integrating Braintree using Hosted Fields

--- orders/views.py
+++ orders/views.py
@@ -24,7 +24,7 @@ def order_create(request):
             # set the order in the session
             request.session['order_id'] = order.id
             # redirect for payment
-            return reverse('payment:process')
+            return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
     return render(request,
